395f25ef8d2a849b1e59be00ac52d9c95b196704
vadlapravallika <64085956+kumareshpv@users.noreply.github.com>
2024-11-30 19:18:47 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css"; // Your custom styles
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Dynamically adding the Google Fonts link to the head
+const link = document.createElement("link");
+link.href =
+  "https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap";
+link.rel = "stylesheet";
+document.head.appendChild(link); // Append the link tag to head
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
