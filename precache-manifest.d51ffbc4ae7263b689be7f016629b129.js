bb3d7280b3fb711ef246667cf1478aa62a43238c
Kumaresh PV <pvkumaresh@gmail.com>
2025-02-17 20:18:18 -0600

    Updates

---
+++ precache-manifest.d51ffbc4ae7263b689be7f016629b129.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c6c1750809ad4bd69dc71b5417d955",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5fa041ed80e0caa98cb4",
+    "url": "/portfolio/static/css/2.4e461ce0.chunk.css"
+  },
+  {
+    "revision": "7bd94a61a28199ca00e9",
+    "url": "/portfolio/static/css/main.6b90eef3.chunk.css"
+  },
+  {
+    "revision": "5fa041ed80e0caa98cb4",
+    "url": "/portfolio/static/js/2.fc7ebf99.chunk.js"
+  },
+  {
+    "revision": "7bd94a61a28199ca00e9",
+    "url": "/portfolio/static/js/main.c54540a5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "495aaafedf58f8fde2c53cdec0ab2024",
+    "url": "/portfolio/static/media/Profile.495aaafe.png"
+  },
+  {
+    "revision": "cccb61434263f733e1224f5b467c6404",
+    "url": "/portfolio/static/media/Project-1.cccb6143.png"
+  },
+  {
+    "revision": "12ad215f9edf1988e47d1878e9e217f2",
+    "url": "/portfolio/static/media/Project-2.12ad215f.png"
+  },
+  {
+    "revision": "cea6bf59a4e8ed3b55af990769544df8",
+    "url": "/portfolio/static/media/Project-3.cea6bf59.png"
+  },
+  {
+    "revision": "c188fa3757c227df06be054e208834c7",
+    "url": "/portfolio/static/media/Project-4.c188fa37.png"
+  },
+  {
+    "revision": "66ea412ac29e0b4054b0042a6248d0ef",
+    "url": "/portfolio/static/media/Project-5.66ea412a.png"
+  },
+  {
+    "revision": "0e9e815c13176af20c738f7ce9f05e17",
+    "url": "/portfolio/static/media/Project-6.0e9e815c.png"
+  }
+]);
